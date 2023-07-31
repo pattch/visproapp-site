be7ff85cd7635c92566fa3016876b7aabdac575c
Sam Sampson <innovatests@gmail.com>
2023-07-30 19:18:21 -0700

    Implement toggling day and night on page and adjust styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'visproapp-site';
+  day = true;
+
+  toggle() {
+    this.day = !this.day;
+  }
 }
