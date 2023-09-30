017160cd28a49f838139a94d7ba95627bb1f8185
Sam Sampson <innovatests@gmail.com>
2023-09-29 20:37:54 -0700

    Begin separating out content into separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SplashComponent } from './splash/splash.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
